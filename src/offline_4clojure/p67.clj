9cda8c6508d8d84a56429b13ef59a7e28f0eb845
Adam Frey <adam@adamfrey.me>
2014-12-22 12:06:46 -0500

    Complete problem 67

--- src/offline_4clojure/p67.clj
+++ src/offline_4clojure/p67.clj
@@ -6,9 +6,17 @@
 (ns offline-4clojure.p67
   (:use clojure.test))
 
-(def __
-;; your solution here
-)
+(defn __ [n]
+  (let [logn (Math/log n)
+        limit (Math/ceil (+ (* n logn) n))]
+    (loop [vals (vec (repeat limit true))
+           base 2]
+      (let [non-primes (range (* base 2) limit base)
+            next-prime-jump (.indexOf (drop base vals) true)
+            next-prime (+ base 1 next-prime-jump)]
+        (if (< -1 next-prime-jump)
+          (recur (reduce #(assoc %1 (- %2 1) false) vals non-primes) next-prime)
+          (take n (drop 1 (map (comp inc first) (filter (comp true? last) (map-indexed vector vals))))))))))
 
 (defn -main []
   (are [soln] soln
