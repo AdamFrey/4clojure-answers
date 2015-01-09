64a9e23c77438e1e1c0377d218ed062b46508a3a
Adam Frey <adam@adamfrey.me>
2015-01-09 08:56:16 -0500

    Complete problem 104

--- src/offline_4clojure/p104.clj
+++ src/offline_4clojure/p104.clj
@@ -6,8 +6,32 @@
   (:use clojure.test))
 
 (def __
-;; your solution here
-)
+  (fn [n]
+    (let [roman-digit
+          (fn [n digit]
+            (let [chars {1 "I"
+                         5 "V"
+                         10 "X"
+                         50 "L"
+                         100 "C"
+                         500 "D"
+                         1000 "M"}
+                  icon (chars digit)
+                  half (chars (* 5 digit))
+                  next-icon (chars (* 10 digit))
+                  n-icon (apply str (repeat n icon))]
+              (if (not next-icon)
+                (apply str (repeat n icon))
+                (cond
+                 (< n 4) n-icon
+                 (= n 4) (str icon half)
+                 (< 4 n 9) (str half (representation (- n 5) digit))
+                 (> n 8) (str icon next-icon)
+                 :else ""))))]
+      (str (roman-digit (quot n 1000) 1000)
+           (roman-digit (quot (rem n 1000) 100) 100)
+           (roman-digit (quot (rem n 100) 10) 10)
+           (roman-digit (rem n 10) 1)))))
 
 (defn -main []
   (are [soln] soln
