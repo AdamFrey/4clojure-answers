9763a59659e2d7f76c793e47f0a9fbfc35e5fbd7
Adam Frey <adam@adamfrey.me>
2015-01-23 08:52:11 -0500

    Complete problem 96

--- src/offline_4clojure/p96.clj
+++ src/offline_4clojure/p96.clj
@@ -6,21 +6,27 @@
   (:use clojure.test))
 
 (def __
-;; your solution here
-)
+  (fn sym
+    ([[v left right]]
+     (sym left right))
+    ([[v1 left1 right1] [v2 left2 right2]]
+     (if (or (nil? v1) (nil? v2))
+       (and (nil? v1) (nil? v2))
+       (and (= v1 v2) (sym left1 right2) (sym right1 left2))))))
 
+(__ [:a [:b :c :d] nil] [:e [:f :g] nil])
 (defn -main []
   (are [soln] soln
-(= (__ '(:a (:b nil nil) (:b nil nil))) true)
-(= (__ '(:a (:b nil nil) nil)) false)
-(= (__ '(:a (:b nil nil) (:c nil nil))) false)
-(= (__ [1 [2 nil [3 [4 [5 nil nil] [6 nil nil]] nil]]
-          [2 [3 nil [4 [6 nil nil] [5 nil nil]]] nil]])
-   true)
-(= (__ [1 [2 nil [3 [4 [5 nil nil] [6 nil nil]] nil]]
-          [2 [3 nil [4 [5 nil nil] [6 nil nil]]] nil]])
-   false)
-(= (__ [1 [2 nil [3 [4 [5 nil nil] [6 nil nil]] nil]]
-          [2 [3 nil [4 [6 nil nil] nil]] nil]])
-   false)
-))
+       (= (__ '(:a (:b nil nil) (:b nil nil))) true)
+       (= (__ '(:a (:b nil nil) nil)) false)
+       (= (__ '(:a (:b nil nil) (:c nil nil))) false)
+       (= (__ [1 [2 nil [3 [4 [5 nil nil] [6 nil nil]] nil]]
+               [2 [3 nil [4 [6 nil nil] [5 nil nil]]] nil]])
+          true)
+       (= (__ [1 [2 nil [3 [4 [5 nil nil] [6 nil nil]] nil]]
+               [2 [3 nil [4 [5 nil nil] [6 nil nil]]] nil]])
+          false)
+       (= (__ [1 [2 nil [3 [4 [5 nil nil] [6 nil nil]] nil]]
+               [2 [3 nil [4 [6 nil nil] nil]] nil]])
+          false)
+       ))
