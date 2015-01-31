2b912ef419c06911b989d469dd53d2a2ff6a4240
Adam Frey <adam@adamfrey.me>
2015-01-31 11:10:20 -0500

    Complete problem 115

--- src/offline_4clojure/p115.clj
+++ src/offline_4clojure/p115.clj
@@ -6,8 +6,11 @@
   (:use clojure.test))
 
 (def __
-;; your solution here
-)
+  (fn [n]
+    (let [st (str n)
+          len (quot (count st) 2)
+          sum-of-str (fn [n-str] (reduce + (map #(- (int %) 48) n-str)))]
+      (= (sum-of-str (take len st)) (sum-of-str (take-last len st))))))
 
 (defn -main []
   (are [soln] soln
@@ -19,5 +22,5 @@
 (= true (__ 89098))
 (= true (__ 89089))
 (= (take 20 (filter __ (range)))
-   [0 1 2 3 4 5 6 7 8 9 11 22 33 44 55 66 77 88 99 101])  
+   [0 1 2 3 4 5 6 7 8 9 11 22 33 44 55 66 77 88 99 101])
 ))
