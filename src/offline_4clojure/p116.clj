576c9ac617517dae7407f7ba883d9132e51e5031
Adam Frey <adam@adamfrey.me>
2015-09-07 17:05:24 -0400

    Complete problem 116

--- src/offline_4clojure/p116.clj
+++ src/offline_4clojure/p116.clj
@@ -6,12 +6,27 @@
   (:use clojure.test))
 
 (def __
-;; your solution here
-)
+  (fn [num]
+    (let [prime? (memoize (fn [x] (and (> x 1)
+                                      (not (some #(zero? (mod x %)) (range 2 x))))))
+          prime-sandwich (fn [lower n upper]
+                           (let [lower-prime? (prime? lower)
+                                 upper-prime? (prime? upper)]
+                             (if (and lower-prime? upper-prime?)
+                               ;; sandwiched between the next two primes?
+                               (= (/ (+ lower upper) 2) n)
+                               ;; keep looking for the next two primes
+                               (if (< lower 1)
+                                 false
+                                 (recur (if lower-prime? lower (dec lower))
+                                        n
+                                        (if upper-prime? upper (inc upper)))))))]
+      (if (prime? num)
+        (prime-sandwich (dec num) num (inc num))
+        false))))
 
 (defn -main []
   (are [soln] soln
-(= false (__ 4))
-(= true (__ 563))
-(= 1103 (nth (filter __ (range)) 15))
-))
+    (= false (__ 4))
+    (= true (__ 563))
+    (= 1103 (nth (filter __ (range)) 15))))
