4a6d673971ed628464ac31c96c434989f003ccab
Adam Frey <adam@adamfrey.me>
2016-09-10 13:15:29 -0400

    Complete problem 131

--- src/offline_4clojure/p131.clj
+++ src/offline_4clojure/p131.clj
@@ -6,33 +6,44 @@
   (:use clojure.test))
 
 (def __
-;; your solution here
-)
+  (fn [& sets]
+    (let [power-set (fn power-set [s]
+                      (if (empty? s)
+                        #{s}
+                        (let [subsets (power-set (set (rest s)))]
+                          (into subsets (map #(conj % (first s)) subsets)))))
+          non-empty-subsets (fn [s]
+                              (filter (comp not empty?) (power-set s)))]
+      (->> sets
+        (map non-empty-subsets)
+        (map (fn [subsets] (map #(reduce + %) subsets)))
+        (map set)
+        (apply clojure.set/intersection)
+        ((comp not empty?))))))
 
 (defn -main []
   (are [soln] soln
-(= true  (__ #{-1 1 99} 
-             #{-2 2 888}
-             #{-3 3 7777})) ; ex. all sets have a subset which sums to zero
-(= false (__ #{1}
-             #{2}
-             #{3}
-             #{4}))
-(= true  (__ #{1}))
-(= false (__ #{1 -3 51 9} 
-             #{0} 
-             #{9 2 81 33}))
-(= true  (__ #{1 3 5}
-             #{9 11 4}
-             #{-3 12 3}
-             #{-3 4 -2 10}))
-(= false (__ #{-1 -2 -3 -4 -5 -6}
-             #{1 2 3 4 5 6 7 8 9}))
-(= true  (__ #{1 3 5 7}
-             #{2 4 6 8}))
-(= true  (__ #{-1 3 -5 7 -9 11 -13 15}
-             #{1 -3 5 -7 9 -11 13 -15}
-             #{1 -1 2 -2 4 -4 8 -8}))
-(= true  (__ #{-10 9 -8 7 -6 5 -4 3 -2 1}
-             #{10 -9 8 -7 6 -5 4 -3 2 -1}))
-))
+    (= true  (__ #{-1 1 99} 
+               #{-2 2 888}
+               #{-3 3 7777})) ; ex. all sets have a subset which sums to zero
+    (= false (__ #{1}
+               #{2}
+               #{3}
+               #{4}))
+    (= true  (__ #{1}))
+    (= false (__ #{1 -3 51 9} 
+               #{0} 
+               #{9 2 81 33}))
+    (= true  (__ #{1 3 5}
+               #{9 11 4}
+               #{-3 12 3}
+               #{-3 4 -2 10}))
+    (= false (__ #{-1 -2 -3 -4 -5 -6}
+               #{1 2 3 4 5 6 7 8 9}))
+    (= true  (__ #{1 3 5 7}
+               #{2 4 6 8}))
+    (= true  (__ #{-1 3 -5 7 -9 11 -13 15}
+               #{1 -3 5 -7 9 -11 13 -15}
+               #{1 -1 2 -2 4 -4 8 -8}))
+    (= true  (__ #{-10 9 -8 7 -6 5 -4 3 -2 1}
+               #{10 -9 8 -7 6 -5 4 -3 2 -1}))))
